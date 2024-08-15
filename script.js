bfa2588f91ddd42a256761efb9d56589225111c1
a <a@as-MacBook-Air.local>
2024-08-15 16:30:39 +0300

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//on searchResultsPageLogoButtonClass click go to searchProductsPage.html
+document.querySelector('.searchResultsPageLogoButtonClass').addEventListener('click', function () {
+    window.location.href = '/searchProductsPage.html';
+});
